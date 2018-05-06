3c4f391ac58ac94a4f6b1133b743a1efe26a744a
Omar Jesus Mejia <omargotico@hotmail.com>
2018-05-06 09:09:21 -0500

    Init Commit

---
+++ playground/mongodb-find.js
@@ -0,0 +1,26 @@
+//const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb'); //Object Desctructuring
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+    if(err){
+        return console.log('Unable to connect to MongoDB server');
+    }
+    console.log('Connected to MongoDB server');
+
+    // db.collection('Todos').find({
+    //     _id: new ObjectID('5aee57387f1ee5bbc95d6f48')
+    // }).toArray().then((docs) => {
+    //     console.log(JSON.stringify(docs, undefined, 2));
+    // }, (err) => {
+    //     console.log('Unable to fetch', err);
+    // });
+
+    db.collection('Todos').find().count().then((count) => {
+        console.log(`Todos count: ${count}`);
+    }, (err) => {
+        console.log('Unable to fetch', err);
+    });
+
+    db.close();
+});
+
