7e887e6c5a62d854e5ecdaa2a43fecdbe91942b1
Omar Mejia <omargotico@hotmail.com>
2018-05-24 16:19:05 -0500

    Added production database route

---
+++ db/db.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+    db_mongo_prod: "mongodb://admin:admin@ds135540.mlab.com:35540/pyme-demo"
+}
--- db/mongoose.js
+++ db/mongoose.js
@@ -1,7 +1,10 @@
 var mongoose = require('mongoose');
 
+const { db_mongo_prod } = require('./db.config');
+
 //Configure mongoose to use built in Promise engine.
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+//mongoose.connect('mongodb://localhost:27017/TodoApp');
+mongoose.connect(db_mongo_prod);
 
 module.exports = {mongoose};
